67161271b81433c4fff436aed22ff5f86ec3e11b
prasan24 <prasanvikram24@gmail.com>
2022-03-16 10:05:15 +0530

    changes done by prasanna for signin & signup page

--- ecommerce-front/src/user/Signup.js
+++ ecommerce-front/src/user/Signup.js
@@ -73,41 +73,41 @@ export default function Signup() {
 
   function signUpForm() {
     return (
-      <form className={classes.root}>
-        <TextField
-          label="Name"
-          onChange={handleChange("name")}
-          variant="outlined"
-          required
-          value={name}
-        />
-        <TextField
-          label="Email"
-          onChange={handleChange("email")}
-          variant="outlined"
-          type="email"
-          required
-          value={email}
-        />
-        <TextField
-          label="Password"
-          variant="outlined"
-          type="password"
-          onChange={handleChange("password")}
-          required
-          value={password}
-        />
-        <div>
-          <Button
-            type="submit"
-            onClick={userSignUp}
-            variant="contained"
-            color="primary"
-          >
-            Signup
-          </Button>
+      <React.Fragment>
+      <div class="flex min-h-screen bg-white">       
+        <div class="md:w-1/2 max-w-lg mx-auto my-24 px-4 py-5 shadow-none">
+          <div class="text-left p-0 font-sans">
+            <h1 class=" text-gray-800 text-3xl font-medium">Create an account</h1>
+          </div>
+          <form action="#" class="p-0">
+            <div class="mt-5">
+              <input type="text" class="block w-full p-2 border rounded border-gray-300 focus:outline-none focus:ring-1 focus:ring-gray-400 focus:border-transparent" onChange={handleChange("name")} placeholder="Name" />
+            </div>
+            <div class="mt-5">
+              <input type="text" class="block w-full p-2 border rounded border-gray-300 focus:outline-none focus:ring-1 focus:ring-gray-400 focus:border-transparent" placeholder="Email" onChange={handleChange("email")} />
+            </div>
+            <div class="mt-5">
+              <input type="password" class="block w-full p-2 border rounded border-gray-300 focus:outline-none focus:ring-1 focus:ring-gray-400 focus:border-transparent" placeholder="Password" onChange={handleChange("password")} />
+            </div>
+
+            <div class="mt-6 block p-5 text-sm md:font-sans text-xs text-gray-800">
+              <input type="checkbox" class="inline-block border-0 " />
+              <span display="inline" class="">By creating an account you are agreeing to our 
+                <a class="" href="/s/terms" target="_blank" data-test="Link">
+                <span class="underline ">Terms and Conditions</span> </a> and
+              <a class="" href="/s/privacy" target="_blank" data-test="Link">
+                <span class="underline">Privacy Policy</span> </a>
+              </span>
+            </div>
+
+            <div class="mt-10">
+              <input type="submit" onClick={userSignUp} value="Sign up with email" class="py-3 bg-green-500 text-white w-full rounded hover:bg-green-600" />
+            </div>
+          </form>
+          <a class="" href="/login" data-test="Link" ><span class="block  p-5 text-center text-gray-800  text-xs ">Already have an account?</span></a>
         </div>
-      </form>
+      </div>
+      </React.Fragment>
     );
   }
 
@@ -120,7 +120,6 @@ export default function Signup() {
       {showError()}
       {showSuccess()}
       {signUpForm()}
-      {JSON.stringify(values)}
     </Layout>
   );
 }
